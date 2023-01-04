855a19a290b2add2a3eb4851b8e351106237fa49
anton2920 <anton2920@gmail.com>
2023-01-04 20:50:36 +0300

    Added 'get_addr()'; reworked code to use it

--- tcp.py
+++ tcp.py
@@ -14,15 +14,27 @@ def init():
 
 def get_host_port(addr: str) -> (str, int):
     addr_parts = addr.split(":")
+    if len(addr_parts) == 1:
+        # NOTE: adding default port value.
+        addr_parts.append(1234)
     assert len(addr_parts) == 2
 
     return addr_parts[0], int(addr_parts[1])
 
 
+def get_addr(host: str, port: int) -> str:
+    return ':'.join((host, str(port)))
+
+
 def listen(addr="127.0.0.1:1234"):
     sock = s.socket(s.AF_INET, s.SOCK_STREAM)
     sock.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
-    sock.bind(get_host_port(addr))
+
+    # NOTE: going back and forth to add port if missing.
+    host, port = get_host_port(addr)
+    addr = get_addr(host, port)
+
+    sock.bind((host, port))
     sock.listen()
 
     log.info(f"Listening on {addr}")
@@ -32,8 +44,12 @@ def listen(addr="127.0.0.1:1234"):
 def dial(addr="127.0.0.1:1234"):
     sock = s.socket(s.AF_INET, s.SOCK_STREAM)
 
+    # NOTE: going back and forth to add port if missing.
+    host, port = get_host_port(addr)
+    addr = get_addr(host, port)
+
     log.info(f"Dialing {addr}...")
-    sock.connect(get_host_port(addr))
+    sock.connect((host, port))
     log.info(f"Connected to {addr}")
 
     return sock
