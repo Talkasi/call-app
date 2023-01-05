36293ed4636b292770782388122380326c4ea932
Talkasi <kitalkasi@gmail.com>
2023-01-05 20:21:20 +0300

    udp implementation added

--- main.py
+++ main.py
@@ -1,11 +1,80 @@
 import argparse
-import logger
 import sound as snd
 import tcp
+import udp
 
+import logger
+import struct
+import pygame.image
+import camera
+import gzip
 from threading import Thread
 
 buffer_size = None
+
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 8080
+MAX_PACK = 65507
+
+
+def get_and_send_data(sock):
+    cam = camera.camera_init()
+
+    try:
+        while True:
+            camera_image = cam.get_image()
+
+            data = pygame.image.tostring(camera_image, 'RGB')
+            data = gzip.compress(bytes(list(data)))
+
+            # Send size of data to receive
+            sock.send(struct.pack("i", len(data)))
+
+            sent_size = 0
+            while sent_size < len(data):
+                size_to_send = MAX_PACK if len(data) - sent_size > MAX_PACK else len(data) - sent_size
+
+                sock.send(data[sent_size:sent_size + size_to_send])
+                sent_size += size_to_send
+
+            if sent_size > len(data):
+                print("[!]Error in sending.")
+                exit(1)
+
+            logger.root_logger.debug(f"Read and sent {len(data)} bytes")
+    except (BrokenPipeError, ConnectionResetError) as e:
+        logger.root_logger.warning(e)
+
+
+def receive_and_play_data(sock, resolution=(1280, 720)):
+    window_display = pygame.display.set_mode(resolution)
+    try:
+        while True:
+            size = int(sock.recv(4).decode("ascii"))
+
+            size_received = 0
+            data = b""
+            while size_received < size:
+                data = sock.recv(size)
+                size_to_receive = MAX_PACK if len(data) - size_received > MAX_PACK else len(data) - size_received
+
+                data += sock.recv(size_to_receive)
+
+            if size_received != size:
+                print("[!]Error in receiving.")
+                exit(1)
+
+            data = gzip.decompress(data).decode("ascii")
+
+            camera_image = pygame.image.fromstring(data, resolution, 'RGB')
+
+            if camera.camera_print_image(camera_image, window_display) == 0:
+                return
+
+            logger.root_logger.debug(f"Read and sent {len(data)} bytes")
+    except (BrokenPipeError, ConnectionResetError) as e:
+        logger.root_logger.warning(e)
 
 
 def read_send(sock):
@@ -48,12 +117,18 @@ def receive_play(sock):
 def start_join_threads(sock):
     read_send_thread = Thread(target=read_send, args=(sock, ))
     receive_play_thread = Thread(target=receive_play, args=(sock, ))
+    receive_and_play_thread = Thread(target=receive_and_play_data, args=(sock, ))
+    get_and_send_thread = Thread(target=get_and_send_data, args=(sock,))
 
     read_send_thread.start()
     receive_play_thread.start()
+    receive_and_play_thread.start()
+    get_and_send_thread.start()
 
     read_send_thread.join()
     receive_play_thread.join()
+    receive_and_play_thread.join()
+    get_and_send_thread.join()
 
     # NOTE: stop steams so no further data is buffered.
     # They will be started back automatically after first read.
@@ -61,7 +136,7 @@ def start_join_threads(sock):
     snd.outstream.stop()
 
 
-def server(addr):
+def tcp_server(addr):
     sock = tcp.listen(addr)
 
     while True:
@@ -72,8 +147,24 @@ def server(addr):
         start_join_threads(peer_sock)
 
 
-def client(addr):
+def tcp_client(addr):
     sock = tcp.dial(addr)
+    start_join_threads(sock)
+
+
+def udp_server(addr):
+    sock = udp.listen(addr)
+
+    while True:
+        peer_sock, (peer_host, peer_port) = sock.accept()
+        peer_addr = udp.get_addr(peer_host, peer_port)
+        udp.log.info(f"Accepted from {peer_addr}")
+
+        start_join_threads(peer_sock)
+
+
+def udp_client(addr):
+    sock = udp.dial(addr)
     start_join_threads(sock)
 
 
@@ -86,8 +177,8 @@ def main():
         "--listen",
         action="store_const",
         dest="handler",
-        default=client,
-        const=server,
+        default=tcp_client,
+        const=tcp_server,
         help="listen on the specified address instead of making connection")
     argparser.add_argument("-v",
                            "--verbose",
@@ -98,6 +189,7 @@ def main():
     logger.init(args.verbose)
     snd.init()
     tcp.init()
+    udp.init()
 
     global buffer_size
     buffer_size = snd.instream.blocksize * 2 * 4  # NOTE: 4 kiB
---
+++ udp.py
@@ -0,0 +1,55 @@
+import socket
+import logging
+
+log = None
+
+
+def init():
+    global log
+
+    assert not log
+
+    log = logging.getLogger("UDP")
+
+
+def get_host_port(addr: str) -> (str, int):
+    addr_parts = addr.split(":")
+    if len(addr_parts) == 1:
+        # NOTE: adding default port value.
+        addr_parts.append(str(4321))
+    assert len(addr_parts) == 2
+
+    return addr_parts[0], int(addr_parts[1])
+
+
+def get_addr(host: str, port: int) -> str:
+    return ':'.join((host, str(port)))
+
+
+def listen(addr="127.0.0.1:4321"):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # NOTE: going back and forth to add port if missing.
+    host, port = get_host_port(addr)
+    addr = get_addr(host, port)
+
+    sock.bind((host, port))
+    sock.listen()
+
+    log.info(f"Listening on {addr}")
+    return sock
+
+
+def dial(addr="127.0.0.1:4321"):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # NOTE: going back and forth to add port if missing.
+    host, port = get_host_port(addr)
+    addr = get_addr(host, port)
+
+    log.info(f"Dialing {addr}...")
+    sock.connect((host, port))
+    log.info(f"Connected to {addr}")
+
+    return sock
