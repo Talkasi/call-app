b914a0398d4e4f9c1331c4529044cf2e641e1ffa
Talkasi <kitalkasi@gmail.com>
2023-01-07 20:48:38 +0300

    Receiving size check 2.0

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import time
+
 import logger
 import sound as snd
 import pygame.image
@@ -35,7 +37,10 @@ def receive_and_play_data(sock, resolution=(1280, 720)):
             data = b''
 
             while len(data) < resolution[0] * resolution[1] * 3:  # Size of an image
-                data += sock.recv(CHUNK_SIZE)
+                received_data = sock.recv(CHUNK_SIZE)
+                if len(received_data) != CHUNK_SIZE:
+                    received_data = received_data + b'\x00' * (CHUNK_SIZE - len(received_data))
+                data += received_data
 
             camera_image = pygame.image.fromstring(data, resolution, 'RGB')
             if camera.camera_print_image(camera_image, window_display) == 0:
